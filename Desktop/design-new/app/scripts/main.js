01523ed225352c0f390a4f98c28059dbf28f8e53
Charlotte Couttie <charlottecouttie@Charlottes-MacBook-Pro-2.local>
2017-05-04 21:40:52 -0700

    first commit

--- Desktop/design-new/app/scripts/main.js
+++
@@ -1,5 +0,0 @@
-
-lightbox = new fusepump.lightbox.buynow(1111);
-var callToActionElement = document.getElementById( "unique-btn");
- callToActionElement.addEventListener("click", function()
-{ lightbox.show(img); //Show the lightbox } );
